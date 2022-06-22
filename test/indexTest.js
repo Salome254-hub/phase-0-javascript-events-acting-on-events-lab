8d5f4274a603fa5b72eb7a3ccbc400e7ff1a8410
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-22 15:22:13 +0300

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,35 +1,30 @@
-describe('moveDodgerLeft()', () => {
-  beforeEach(() => {
-    dodger = document.getElementById('dodger')
-  })
+// Your code here
+const dodger = document.getElementById("dodger"); 
 
-  it('moves the DODGER to the left', () => {
-    var left = dodger.style.left
-    left = parseInt(left)
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
 
-    moveDodgerLeft()
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
 
-    var newPosition = dodger.style.left
-    newPosition = parseInt(newPosition)
+function moveDodgerRight() {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
 
-    expect(newPosition).to.be.below(left)
-  })
-})
-
-describe('moveDodgerRight()', () => {
-  beforeEach(() => {
-    dodger = document.getElementById('dodger')
-  })
-
-  it('moves the DODGER to the right', () => {
-    var left = dodger.style.left
-    left = parseInt(left)
-
-    moveDodgerRight()
-
-    var newPosition = dodger.style.left
-    newPosition = parseInt(newPosition)
-
-    expect(newPosition).to.be.above(left)
-  })
-})
+      if (left < 360){
+          dodger.style.left = `${left + 1}px`
+      }
+}
+ document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+}); 
